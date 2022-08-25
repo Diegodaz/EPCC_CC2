7982f1010cae416e73aad62976458bdcea7adabe
Diego Alonso Zanabria Sacsi <83051094+Diegodaz@users.noreply.github.com>
2022-08-24 23:51:43 -0500

    Add files via upload

---
+++ TerceraUnidad/Semana15/e3asignacion/e3asignacion.cpp..cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "LinkedList.h"
+using namespace std;
+int main(){
+	LinkedList l1;
+	for(int i{0}; i < 10; i+=2)
+    {
+        l1.insert(i);
+    }
+	std::cout << "\nPrimera lista:\n";
+	std::cout << l1;
+    LinkedList l2;
+    l2 = l1;
+    std::cout << "\n\nLista copiada:\n";
+	std::cout << l2;
+	return 0;
+}
